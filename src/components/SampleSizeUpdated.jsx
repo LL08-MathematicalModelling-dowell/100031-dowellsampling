d50e2bcff7652c9a0ffef5584ad604464f0a6b28
Hammad Rao <106843117+hammadrao891@users.noreply.github.com>
2023-09-19 23:51:16 +0500

    Update SampleSizeUpdated.jsx

--- src/components/SampleSizeUpdated.jsx
+++ src/components/SampleSizeUpdated.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import ClipLoader from "react-spinners/ClipLoader";
 import { Checkbox } from '@mui/material'
 import { useState } from 'react'
+import "./samplesize.css"
 import { Table, TableHead, TableBody, TableRow, TableCell } from '@mui/material';
 const SampleSizeUpdated = () => {
     const [type,setType] = useState("select")
     const [sKnown,setSKnown] = useState(false)
     const [standard_deviation,set_standard_deviation]=useState(null)
+    const [conf,set_conf]=useState(false)
     const [population_size,set_population_size] = useState(null)
-    const [error,setError]=useState(0.5)
+    const [error,setError]=useState(0)
     const [confidence_level,set_confidence_level]=useState(0.90)
     const [toggle,setToggle] = useState(false)
     const [buttonClicked,setButtonClicked] = useState(false)
@@ -17,14 +19,27 @@ const SampleSizeUpdated = () => {
 {
  
 setButtonClicked(true)
-var data =
+
+var data;
+if(conf){
+ data =
     {
     population_size,
     error,
     standard_deviation,
     confidence_level 
     }
-// console.log(data)
+  }
+  else if(!conf)
+  {
+    data =
+    {
+    population_size,
+    error,
+    standard_deviation,
+    }
+  }
+console.log(data)
  fetch("https://100102.pythonanywhere.com/sample-size/", {
   method: "POST",
   headers: {
@@ -59,7 +74,7 @@ var data =
 
   return (
     <div>
-        <div style={{display:"block",justifyContent:"center",marginTop:"5em",gap:"0.5em",width:"70%",marginLeft:"10em"}}>
+        <div className='mobile'>
          <h1 >Sample Size</h1>
          <p>Type:</p>
     <select className="form-control form-control-lg" onChange={e=>setType(e.target.value)}>
@@ -67,31 +82,40 @@ var data =
         <option value="finite">Finite Population</option>
         <option value="infinite">Infinite Population</option>
     </select>
-        <div style={{display:"flex",justifyContent:"space-between"}}>
+        <div style={{display:"flex",padding:"1em",justifyContent:"space-between",gap:"5em"}}>
             <div style={{width:"50%",display:"grid",gap:"1em"}}>
                 { type !== "select" &&
                 <>
                 {type === "finite" &&
                 <>
-                    <label className="form-label">Population Size</label>
+                    <label className="form-label">Population Size:</label>
       <input  class="form-control" placeholder='Enter N' onChange={e=>set_population_size(e.target.value)} type='number' />
       </> }
         <div style={{display:"flex"}}> 
          <label>Standard Deviation:</label>
-        <Checkbox   onChange={e=>setSKnown(!sKnown)}/>
+        <Checkbox  style={{marginTop:"-9px"}} onChange={e=>setSKnown(!sKnown)}/>
         </div>
             {
                 sKnown &&
                 <input class="form-control" placeholder='Enter Standard Deviation' onChange={e=>set_standard_deviation(e.target.value)} type='number'/>}
-      <label className="form-label">Error</label>
-       <input type='number' className="form-control form-control-lg" placeholder='Error'  style={{width:"200px"}}/>
+      <label className="form-label">Error:</label>
+       <input type='number' className="form-control form-control-lg" placeholder='Error'  style={{width:"200px"}} onChange={e=>setError(e.target.value)}/>
+         <div style={{display:"flex"}}> 
+         <label>Confidence Level:</label>
+        <Checkbox  style={{marginTop:"-9px"}} onChange={e=>set_conf(!conf)}/>
+        </div>
+       {
+        conf && <>
+        <label className="form-label">Confidence Level:</label>
        <select className="form-control form-control-lg" onChange={(e)=>set_confidence_level(e.target.value)}>
         <option disabled>--select--</option>
         <option>0.90</option>
         <option>0.95</option>
         <option>0.99</option>
         </select>
-        <button onClick={handleSubmit} className="btn btn-primary">Submit</button>
+        </>
+        }
+        <button onClick={handleSubmit} className="btn btn-primary" disabled={buttonClicked} >Submit</button>
   </>
   }     
        </div>
@@ -135,6 +159,11 @@ var data =
       </div>
  
  }
+ {toggle &&
+    <>
+        <button onClick={e=>window.location.reload("/")} className="btn btn-primary"  >Try Again</button>
+    </>
+    }
         </div>
       
     </div>
