d1935411c6f2d8b8a24e168e1e401b565a396f47
denniskanyugira <109817995+denniskanyugira@users.noreply.github.com>
2023-09-25 22:01:46 +0300

    Two stage sampling Implemented

--- Sampling/API/urls.py
+++ Sampling/API/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.samplingInternalAPI, name = "Internal Sampling API"),
-    path('<str:api_key>/',views.samplingAPI, name = "Public Sampling API")
+    path('<str:api_key>/',views.samplingAPI, name = "Public Sampling API"),
+    path('two-stage-sampling/',views.dowelltwostagesampling, name = "two-stage-sampling")
 ]
