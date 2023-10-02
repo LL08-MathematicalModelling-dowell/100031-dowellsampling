e0556a323af6e35c651d8e11c90bd38bfbcf7a62
denniskanyugira <109817995+denniskanyugira@users.noreply.github.com>
2023-10-02 08:59:04 +0300

    Updated two stage sampling endpoint for testing

--- Sampling/API/urls.py
+++ Sampling/API/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.samplingInternalAPI, name = "Internal Sampling API"),
-    path('<str:api_key>/',views.samplingAPI, name = "Public Sampling API"),
-    path('two-stage-sampling/',views.dowelltwostagesampling, name = "two-stage-sampling")
+    path('', views.samplingInternalAPI, name="Internal Sampling API"),
+    path('<str:api_key>/', views.samplingAPI, name="Public Sampling API"),
+    path('two-stage-sampling/', views.dowell_twostage_sampling_view, name="two-stage-sampling")
 ]
