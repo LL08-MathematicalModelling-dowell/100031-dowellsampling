44409353e8db0f0876169ef372d847dfc1885332
gitone912 <akash.101@kvsroranchi.in>
2023-11-08 01:11:30 +0530

    Sample_size_fix

--- Sampling/SampleSizeApi/functions/z_score.py
+++ Sampling/SampleSizeApi/functions/z_score.py
@@ -1,9 +1,12 @@
+from scipy.stats import norm
+
 def get_z_score(confidence_level):
-    if confidence_level == 0.90:
-        return 1.645
-    elif confidence_level == 0.95:
-        return 1.96
-    elif confidence_level == 0.99:
-        return 2.58
-    else:
-        raise ValueError("Invalid confidence level. Supported values: 0.90, 0.95, 0.99")
+    alpha = 1 - confidence_level
+
+    # For a two-tailed test, divide alpha by 2
+    alpha /= 2
+
+    # Find the critical z-value
+    critical_z = norm.ppf(1 - alpha)
+
+    return critical_z
